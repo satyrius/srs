c16026aae7b17cc3bd97c54b154911f06d4d6c61
Anton Egorov <anton.egoroff@gmail.com>
2016-11-29 09:53:11 +0300

    hosted zone as a template parameter

--- static_site.py
+++ static_site.py
@@ -4,27 +4,37 @@ from troposphere import Parameter, Template
 from troposphere import Ref, GetAtt, Join
 from troposphere import constants as c, s3, cloudfront, route53
 
+# TODO get CloudFront hosted zone id from a list (use lambda function)
 CLOUDFRONT_HOSTED_ZONE_ID = 'Z2FDTNDATAQYW2'
 
 
 template = Template()
 
 
+# TODO create lambda fnction to get zone from a list
+zone = template.add_parameter(Parameter(
+    'WebsiteDomainZone',
+    Type=c.STRING,
+    Default='example.com',
+    Description='A root hosted zone for your website domain',
+))
+
 domain = template.add_parameter(Parameter(
     'WebsiteDomainName',
     Type=c.STRING,
-    Description='A fully qualified domain name for your website (without www)',
+    Default='subdomain.example.com',
+    Description='A fully qualified domain name for your website inside a the hosted zone',
 ))
 
 index_page = template.add_parameter(Parameter(
-    'WebsiteIndexPage',
+    'WebsitePageIndex',
     Type=c.STRING,
     Default='index.html',
     Description='Index page for your site',
 ))
 
 error_page = template.add_parameter(Parameter(
-    'WebsiteErrorPage',
+    'WebsitePageError',
     Type=c.STRING,
     Default='error.html',
     Description='Error page for your site',
@@ -76,13 +86,13 @@ cdn = template.add_resource(cloudfront.Distribution(
     )
 ))
 
-hosted_zone = Join('', [Ref(domain), '.'])
+hosted_zone = Join('', [Ref(zone), '.'])
 template.add_resource(route53.RecordSetGroup(
     'WebsiteDNSRecord',
     HostedZoneName=hosted_zone,
     Comment='Records for the root of the hosted zone',
     RecordSets=[route53.RecordSet(
-        Name=hosted_zone,
+        Name=Ref(domain),
         Type='A',
         AliasTarget=route53.AliasTarget(
             CLOUDFRONT_HOSTED_ZONE_ID,
